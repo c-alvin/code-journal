f73f7f6650469ab957b32cc7072be4c5b7123217
Alvin Cho <alvinxcho2@gmail.com>
2022-07-18 21:52:19 +0000

    listen for input completed, working on submit events

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-// var $photoUrlInput = document.querySelector('.photourl');
+var $photoUrlInput = document.querySelector('.photourl');
+var $img = document.querySelector('.imageurl');
 
-// $photoUrlInput.addEventListener('input', photoInput);
+function photoInput(event) {
+  if (event.target.matches('.photourl')) {
+    $img.setAttribute('src', event.target.value);
+  }
+}
+
+$photoUrlInput.addEventListener('input', photoInput);
+
+var $form = document.querySelector('.form');
+
+function formSubmit(event) {
+  var objform = {};
+  objform.title = $form.elements.title.value;
+  objform.photourl = $form.elements.photo.value;
+  objform.notes = $form.elements.note.value;
+  objform.nextEntryId = data.nextEntryId;
+
+}
+
+$form.addEventListener('submit', formSubmit);
