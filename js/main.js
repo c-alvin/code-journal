5198523c335d0d969e3030bfd605dc4d05fe94dd
Alvin Cho <alvinxcho2@gmail.com>
2022-07-18 23:51:28 +0000

    Completed issue-1-create-an-entry

--- js/main.js
+++ js/main.js
@@ -17,12 +17,11 @@ function formSubmit(event) {
   objform.title = $form.elements.title.value;
   objform.photourl = $form.elements.photo.value;
   objform.notes = $form.elements.note.value;
-  objform.nextEntryId = data.nextEntryId;
+  objform.EntryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(objform);
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
-
 }
 
 $form.addEventListener('submit', formSubmit);
